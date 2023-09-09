d706bc6d5f9b39469db4e8ddb0e786ea95dede20
Amrit Niure <niureamrit2@gmail.com>
2023-09-09 23:25:52 +1000

    you have to get it dude..3

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -15,7 +15,7 @@ const [posts, setPosts] = useState([])
 useEffect(() =>{
   const fetchData = async () => {
     try {
-      const posts = await fetch("https://amritniure.com.np/api/posts");
+      const posts = await fetch("https://www.amritniure.com.np/api/posts");
       const jsonPost = await posts.json();
       setPosts(jsonPost.posts)
     } catch (error) {
