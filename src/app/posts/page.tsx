894f501859eadb07a763c2b678ec3ede67ffea44
Amrit Niure <niureamrit2@gmail.com>
2023-09-11 10:24:57 +1000

    Morning .....

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -16,7 +16,7 @@ const [posts, setPosts] = useState([])
 useEffect(() =>{
   const fetchData = async () => {
     try {
-      const posts = await fetch(`http://localhost:3000/api/posts/`,{
+      const posts = await fetch(`api/posts/`,{
       // const posts = await fetch(`api/posts`,{
         cache : "no-store"
       });
