e6c8fdb0c09985e721c66bb03545e9aff26fa133
Amrit Niure <niureamrit2@gmail.com>
2023-09-12 14:52:55 +1000

    Commit ..

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -34,7 +34,7 @@ const Post: FC<PostProps> = async ({ params }) => {
 
   return (
     <div className="flexbox ">
-      {/* <div className="container pt-[5vh] flex flex-col md:flex-row w-full md:gap-8 ">
+      <div className="container pt-[5vh] flex flex-col md:flex-row w-full md:gap-8 ">
         <div className="text-primary dark:text-primary-light flex flex-col gap-4 md:w-[70%]">
           <span className="bg-primary dark:bg-light-dark-background text-primary-light px-4 py-2 rounded-md w-fit">
             {postData.category}
@@ -75,11 +75,11 @@ const Post: FC<PostProps> = async ({ params }) => {
             alt="laptop photo"
             className="rounded-md self-center"
           />
-          <div>{formatContent(postData.description[2])}</div> */}
+          <div>{formatContent(postData.description[2])}</div>
           {/* <div className="bg-primary-dark px-2 text-white dark:bg-black rounded-md">
             <CodeBlock language="javascript" code={postData.code[2]} fileName="abc.txt" />
           </div> */}
-        {/* </div>
+        </div>
         <div className="md:h-[80vh] md:w-[30%] flex items-center py-16 md md:py-0">
           <div className=" w-full text-primary dark:text-primary-light">
             <span className="text-h2 font-semibold ">Read More On</span>
@@ -117,7 +117,7 @@ const Post: FC<PostProps> = async ({ params }) => {
             </ul>
           </div>
         </div>
-      </div> */}
+      </div>
       hello
     </div>
   );
