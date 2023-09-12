cf8385f15381ca83d4dc6b3deddf1e6669a59986
Amrit Niure <niureamrit2@gmail.com>
2023-09-12 14:24:59 +1000

    Commit ..

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -7,17 +7,17 @@ export async function GET(req: Request, { params }: { params: { postId: string }
         await connectionDB()
         const { postId } = params
 
-        if (!postId) {
-            return NextResponse.json({ success: false, message: 'postId is required' }, { status: 400 });
-        }
-        // Find the post by postId
-        const post = await Post.findById(postId);
+        // if (!postId) {
+        //     return NextResponse.json({ success: false, message: 'postId is required' }, { status: 400 });
+        // }
+        // // Find the post by postId
+        // const post = await Post.findById(postId);
 
-        if (!post) {
-            return NextResponse.json({ success: false, message: 'Post not found' }, { status: 400 });
-        }
+        // if (!post) {
+        //     return NextResponse.json({ success: false, message: 'Post not found' }, { status: 400 });
+        // }
 
-        return NextResponse.json({ success: true, post: post }, { status: 200 });
+        return NextResponse.json({ success: true, post: 'post' }, { status: 200 });
 
     } catch (error) {
         console.log(error)
